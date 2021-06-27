a23d2e611cf5269cf62eaba0b3ae81481e25c67b
bilenkobogdan1 <56382520+bilenkobogdan1@users.noreply.github.com>
2021-06-27 21:58:38 +0300

    Add files via upload

---
+++ Webgood1/Webgood1/Models/Greadbook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgood1.Models
+{
+    public class Greadbook
+    {
+       public Greadbook()
+        {
+            Student = new List<Students>();
+        }
+
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        
+        public string Mark { get; set; }
+        public string Subject { get; set; }
+        public virtual Group Group { get; set; }
+        public virtual ICollection<Students> Student { get; set; }
+
+        
+    }
+}
---
+++ Webgood1/Webgood1/Models/Students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgood1.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public int GreadebookId { get; set; }
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Pasword { get; set; }
+        public string YearOfStudy { get; set; }
+        public virtual Greadbook Greadbook { get; set; }
+        public virtual Group Group { get; set; }
+
+    }
+}
