1364ec3693c06bf1ba832d918695f0d2d8464cb9
bilenkobogdan1 <56382520+bilenkobogdan1@users.noreply.github.com>
2021-06-27 21:48:53 +0300

    Add files via upload

---
+++ Webgood1/Webgood1/Models/Lab2LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Webgood1.Models
+{
+    public class Lab2LibraryContext:DbContext
+    {
+        public virtual DbSet<Coments> Coment { get; set; }
+        public virtual DbSet<Greadbook> Greadbooks { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<Students> Student { get; set; }
+        public virtual DbSet<Teacher> Teachers { get; set; }
+
+        public Lab2LibraryContext(DbContextOptions<Lab2LibraryContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
